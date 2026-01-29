c1b1c3aed42df6b20a78cdbb1f754128c916f4c7
ishanpagariya <ishanpagariya1@gmail.com>
2026-01-29 23:05:53 +0530

    main working code

--- internship2.py
+++ internship2.py
@@ -2,15 +2,16 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
+import tf_keras as keras
 from google import genai
 
 client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
 
-model = keras.models.load_model("keras_model.h5", compile=False)
+model = keras.models.load_model("keras_model.h5")
 
 
 st.title("Everyday Object Sustainability ♻️")
+labels = ["Highly sustainable","Moderately sustainable","Non sustainable","Compostable"]
 
 image = st.camera_input("Take a photo of waste")
 
@@ -23,26 +24,19 @@ if image:
     with st.spinner("Analyzing waste..."):
         prediction = model.predict(img_array)
 
-    labels = ["Highly sustainable","Moderately sustainable","Non sustainable","Compostable"]
-
     class_index = np.argmax(prediction)
     predicted_class = labels[class_index]
 
-    st.success(f"This looks like it is: **{predicted_class}**")
+    st.success(f"This looks like it is: {predicted_class}")
 
     with st.spinner("Thinking..."):
         response = client.models.generate_content(
             model="gemini-3-flash-preview",
-            contents=(
-                f"Explain in one sentence how {predicted_class} can be treated"
-                
-            )
+            contents=(f"The user photographed an item categorized as '{predicted_class}'. Give one specific tip on how to dispose of or reuse such items.")
         )
 
     if response and hasattr(response, "candidates") and response.candidates:
-     text = response.candidates[0].content.parts[0].text
-     st.write(text)
+        text = response.candidates[0].content.parts[0].text
+        st.write(text)
     else:
         st.write("Could not generate a response. Try again.")
-
-              
